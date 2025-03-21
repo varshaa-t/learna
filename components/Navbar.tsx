9c4aba7a44ffa8e3266fb650f9748a94a2216732
Varshaa <vvarshaas@gmail.com>
2025-03-21 15:25:53 +0530

    animations for Navbar and HeroSection

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,24 @@ import { dmSansFont } from "@/fonts/fonts";
 import Link from "next/link";
 import MenuIcon from "@/icons/MenuIcon";
 import CloseIcon from "@/icons/CloseIcon";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Navbar(){
 
     const [isOpen, setIsOpen] = useState(false);
+    const navbarLogoOneRef = useRef<HTMLAnchorElement | null>(null);
+    const navbarHomeOneRef = useRef<HTMLAnchorElement | null>(null);
+    const navbarButtonOneRef = useRef<HTMLAnchorElement | null>(null);
+    const navbarLogoTwoRef = useRef<HTMLAnchorElement | null>(null);
+    const navbarButtonTwoRef = useRef<HTMLButtonElement | null>(null);
+    const navbarLogoThreeRef = useRef<HTMLAnchorElement | null>(null);
+    const navbarButtonThreeRef = useRef<HTMLButtonElement | null>(null);
+    const navbarHomeTwoRef = useRef<HTMLAnchorElement | null>(null);
+    const navbarButtonFourRef = useRef<HTMLAnchorElement | null>(null);
 
     function toggleNavbar(){
         setIsOpen(!isOpen);
@@ -28,10 +41,46 @@ export default function Navbar(){
         };
     }, [isOpen]);
 
+    useGSAP(() => {
+        if(!navbarLogoOneRef.current || !navbarHomeOneRef.current || !navbarButtonOneRef.current) return ;
+
+        gsap.from([navbarLogoOneRef.current, navbarHomeOneRef.current, navbarButtonOneRef.current], {
+            opacity: 0,
+            y: 25,
+            autoAlpha: 0
+        })
+    }, []);
+
+    useGSAP(() => {
+        if(!navbarLogoTwoRef.current || !navbarButtonTwoRef.current) return ;
+
+        gsap.from([navbarLogoTwoRef.current, navbarButtonTwoRef.current], {
+            opacity: 0,
+            y: 25,
+            autoAlpha: 0
+        })
+    }, [isOpen]);
+
+    useGSAP(() => {
+        if(!navbarLogoThreeRef.current || !navbarButtonThreeRef.current || !navbarHomeTwoRef.current || !navbarButtonFourRef.current) return ;
+        
+        gsap.from([navbarLogoThreeRef.current, navbarButtonThreeRef.current, navbarHomeTwoRef.current, navbarButtonFourRef.current], {
+            opacity: 0,
+            y: 25,
+            stagger: 0.1,
+            autoAlpha: 0
+        })
+        
+    }, [isOpen])
+
     return (
         <nav className={`bg-tertiary-orange ${dmSansFont.className}`}>
             <div className={`hidden md:flex justify-between items-center px-11 pt-11`}>
-                <Link href={"/"}>
+                <Link 
+                    className="invisible"
+                    ref={navbarLogoOneRef}
+                    href={"/"}
+                >
                     <Image
                         src={"/images/logo.png"}
                         alt="logo"
@@ -40,12 +89,20 @@ export default function Navbar(){
                     />  
                 </Link>
                 <div className="hidden md:flex items-center space-x-6">
-                    <Link href={'/'}>
+                    <Link 
+                        className="invisible"
+                        ref={navbarHomeOneRef}
+                        href={'/'} 
+                    >
                         <div className="font-semibold">
                             Home
                         </div>
                     </Link>
-                    <Link href={'/demo'}>
+                    <Link 
+                        className="invisible"
+                        ref={navbarButtonOneRef}
+                        href={'/demo'}
+                    >
                         <Button
                             text="Get a Demo"
                             size="md"
@@ -55,8 +112,12 @@ export default function Navbar(){
                 </div>
             </div>
             {!isOpen && (
-                <div className="flex items-center justify-end pt-10 space-x-[19vw] px-4 xs:px-6 xs:space-x-[27vw] sm:px-11 sm:space-x-[28vw] md:hidden md:space-x-[28vw] md:pt-12 lg:pt-11 lg:space-x-[34vw] xl:hidden">
-                    <Link href={"/"}>
+                <div className="flex items-center justify-end pt-10 space-x-[19vw] px-4 xs:px-6 xs:space-x-[27vw] sm:px-11 sm:space-x-[28vw] md:hidden md:space-x-[28vw] md:pt-12 lg:pt-11 lg:space-x-[34vw]">
+                    <Link 
+                        className="invisible"
+                        ref={navbarLogoTwoRef}
+                        href={"/"}
+                    >
                         <div className="w-[120px] md:w-auto md:h-auto">
                             <Image
                                 src={"/images/logo.png"}
@@ -67,16 +128,22 @@ export default function Navbar(){
                         </div>
                     </Link>
                     <button 
-                            onClick={toggleNavbar}
-                        >
-                            {isOpen ? <CloseIcon size={32}/> : <MenuIcon/>}
+                        className="invisible"
+                        ref={navbarButtonTwoRef}
+                        onClick={toggleNavbar}
+                    >
+                        {isOpen ? <CloseIcon size={32}/> : <MenuIcon/>}
                     </button>
                 </div>
             )}
             {isOpen && (
                 <div className="fixed inset-0 z-50 flex flex-col justify-between items-center xl:hidden bg-[#f5ede3] pt-11 px-11 pb-16">
                     <div className="flex justify-end items-center">
-                        <Link href={"/"}>
+                        <Link 
+                            ref={navbarLogoThreeRef} 
+                            className="invisible" 
+                            href={"/"}
+                        >
                             <Image
                                 src={"/images/logo.png"}
                                 alt="logo"
@@ -86,18 +153,27 @@ export default function Navbar(){
                             />  
                         </Link>
                         <button 
+                            ref={navbarButtonThreeRef}
                             onClick={toggleNavbar}
-                            className="block absolute right-8 sm:right-10 lg:top-14 xl:hidden"
+                            className="invisible block absolute right-8 sm:right-10 lg:top-14 xl:hidden"
                         >
                             {isOpen ? <CloseIcon size={28}/> : <MenuIcon/>}
                         </button>
                     </div>
-                    <Link href={'/'}>
+                    <Link 
+                        className="invisible"
+                        ref={navbarHomeTwoRef}
+                        href={'/'}
+                    >
                         <div className="text-2xl tracking-tighter">
                             Home
                         </div>
                     </Link>
-                    <Link href={'/demo'}>
+                    <Link 
+                        className="invisible"
+                        ref={navbarButtonFourRef}
+                        href={'/demo'}
+                    >
                         <Button
                             text="Get a Demo"
                             size="lg"
--- sections/HeroSection.tsx
+++ sections/HeroSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Button from "@/components/Button";
 import StoreButton from "@/components/Store-Button";
 import AppleIcon from "@/icons/AppleIcon";
@@ -5,8 +7,60 @@ import GooglePlayIcon from "@/icons/GooglePlayIcon";
 import Image from "next/image";
 import { dmSansFont, manropeFont } from "@/fonts/fonts";
 import Link from "next/link";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function HeroSection(){
+
+    const heroSectionTitleRef = useRef<HTMLDivElement | null>(null);
+    const heroSectionTextRef = useRef<HTMLDivElement | null>(null);
+    const heroSectionImageRef = useRef<HTMLImageElement | null>(null);
+    const heroSectionButtonOneRef = useRef<HTMLAnchorElement | null>(null);
+    const heroSectionButtonTwoRef = useRef<HTMLDivElement | null>(null);
+
+    useGSAP(() => {
+        if(!heroSectionTitleRef.current || !heroSectionTextRef.current || !heroSectionImageRef.current) return ;
+
+        gsap.from(heroSectionTitleRef.current, {
+            opacity: 0,
+            autoAlpha: 0,
+            y: 25
+        })
+
+        gsap.from(heroSectionTextRef.current, {
+            opacity: 0,
+            autoAlpha: 0,
+            duration: 1
+        })
+
+        gsap.from(heroSectionImageRef.current, {
+            opacity: 0,
+            autoAlpha: 0,
+            y: 200,
+            ease: "slow",
+            duration: 0.5
+        })
+    }, []);
+
+    useGSAP(() => {
+        if(!heroSectionButtonTwoRef.current) return ;
+
+        gsap.from([heroSectionButtonOneRef.current, heroSectionButtonTwoRef.current], {
+            opacity: 0,
+            autoAlpha: 0,
+            y: 25,
+            scrollTrigger: {
+                trigger: heroSectionButtonTwoRef.current,
+                start: "top 100%"
+            }
+        })
+
+        ScrollTrigger.refresh();
+    }, []);
 
     return (
         <div className="bg-tertiary-orange border-b-2 border-black flex flex-col md:flex-row md:items-end md:justify-between 
@@ -15,13 +69,25 @@ export default function HeroSection(){
         >
             <div className="flex flex-col justify-between space-y-24 md:space-y-5 lg:space-y-6 2xl:space-y-20">
                 <div className="flex flex-col space-y-[450px] md:space-y-5 lg:space-y-16">
-                    <div className={`flex flex-col text-3xl xs:text-4xl lg:text-5xl xl:text-6xl 2xl:text-[65px] space-y-7 md:space-y-8 lg:space-y-7 ${manropeFont.className}`}>
+                    <div    
+                        ref={heroSectionTitleRef}
+                        className={`invisible flex flex-col text-3xl xs:text-4xl lg:text-5xl xl:text-6xl 2xl:text-[65px] space-y-7 md:space-y-8 lg:space-y-7 ${manropeFont.className}`}
+                    >
                         <div className="relative md:block w-72 md:w-52 lg:w-auto">Work smarter,</div>
                         <div className="w-80 lg:w-auto">not harder</div>
                     </div>
-                    <div className={`max-w-full pr-8 md:pr-0 md:max-w-60 lg:max-w-[350px] xl:max-w-sm 2xl:max-w-lg text-md lg:text-lg 2xl:text-xl lg:tracking-wide font-semibold ${dmSansFont.className}`}>Learna is a microlearning app designed to transform how people learn, grow and engage at work. Focused on the most in-demand soft skills and supported by actionable insights, Learna makes it simple to build a future-fit workforce.</div>
+                    <div 
+                        ref={heroSectionTextRef}
+                        className={`invisible max-w-full pr-8 md:pr-0 md:max-w-60 lg:max-w-[350px] xl:max-w-sm 2xl:max-w-lg text-md lg:text-lg 2xl:text-xl lg:tracking-wide font-semibold ${dmSansFont.className}`}
+                    >
+                        Learna is a microlearning app designed to transform how people learn, grow and engage at work. Focused on the most in-demand soft skills and supported by actionable insights, Learna makes it simple to build a future-fit workforce.
+                    </div>
                 </div>
-                <Link href={'/demo'}>
+                <Link 
+                    ref={heroSectionButtonOneRef}
+                    className="invisible"
+                    href={'/demo'}
+                >
                     <Button
                         text="Get a Demo"
                         size="2xl"
@@ -31,14 +97,18 @@ export default function HeroSection(){
             </div>
             <div>
                 <Image
-                    className="absolute top-[310px] left-0 xs:left-[7vw] sm:left-[20vw] w-[400px] md:top-[50vh] md:left-[50vw] md:w-[340px] lg:top-[40vh] lg:left-[48vw] lg:w-[480px] xl:static xl:w-[600px] 2xl:w-[650px] h-auto"
+                    ref={heroSectionImageRef}
+                    className="invisible absolute top-[310px] left-0 xs:left-[7vw] sm:left-[20vw] w-[400px] md:top-[50vh] md:left-[50vw] md:w-[340px] lg:top-[40vh] lg:left-[48vw] lg:w-[480px] xl:static xl:w-[600px] 2xl:w-[650px] h-auto"
                     src={"/images/hero-section-image.png"}
                     alt="explore-section-on-app"
                     width={670}
                     height={659}
                 />
             </div>
-            <div className="absolute top-[1100px] sm:top-[1050px] sm:left-[6vw] md:left-auto md:top-[750px] lg:top-[780px] xl:top-[830px] 2xl:top-[900px] flex space-x-4">
+            <div 
+                ref={heroSectionButtonTwoRef}
+                className="invisible absolute top-[1100px] sm:top-[1050px] sm:left-[6vw] md:left-auto md:top-[750px] lg:top-[780px] xl:top-[830px] 2xl:top-[900px] flex space-x-4"
+            >
                 <StoreButton
                     icon={<AppleIcon/>}
                     textOne="GET IT ON"
